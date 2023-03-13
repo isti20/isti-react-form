715b7fbb14544e0ab4159992d5b317659339ef3e
iis istiqomah <iisistiqomah20@gmail.com>
2023-03-13 09:05:11 +0700

    styling form

--- src/component/LoginForm/LoginForm.jsx
+++ src/component/LoginForm/LoginForm.jsx
@@ -2,8 +2,9 @@ import {Container, Form, Button} from 'react-bootstrap';
 
 function Login() {
     return (
-        <Container className='l-container d-flex justify-content-center'>
+        <Container>
             <Form>
+                <h2 className="mb-3">Login Form</h2>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email" />
@@ -12,9 +13,6 @@ function Login() {
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
